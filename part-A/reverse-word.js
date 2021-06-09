5538a7d2fc26e661139d5b85f7bc189b22220bf7
hex <chenhexin612@gmail.com>
2021-06-10 02:34:34 +1000

    finished question 2

---
+++ part-A/reverse-word.js
@@ -0,0 +1,45 @@
+function reverseWords(target) {
+    try {
+        if (!target || target.length === 0) {
+            throw new Error('string is empty!');
+        }
+        if (target.length === 1) {
+            return target;
+        }
+        let pointerA = 0;
+        let pointerB;
+        const words = target.split('');
+        for (let i = 0; i <= words.length; i++) {
+            if (words[i] === ' ' || !words[i]) {
+                pointerB = i - 1;
+
+                while (pointerA < pointerB) {
+                    let temp;
+                    temp = words[pointerA];
+                    words[pointerA] = words[pointerB];
+                    words[pointerB] = temp;
+                    pointerA++;
+                    pointerB--;
+
+                }
+                pointerA = i + 1;
+                pointerB = i + 1;
+            }
+
+        }
+        let result = '';
+        for (let i = 0; i < words.length; i++) {
+            result += words[i];
+        }
+        return result;
+    } catch (e) {
+        console.log("Error: ", e);
+    }
+
+
+
+}
+
+let str = "cat and dog";
+console.log("Before: ", str);
+console.log("After: ", reverseWords(str));
